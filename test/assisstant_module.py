8fda932f883a5a851162d39a31f7596498b7a476
PrePolar <jkh6912@gmail.com>
2019-05-16 22:13:07 +0900

    Update assisstant_module.py

--- test/assisstant_module.py
+++ test/assisstant_module.py
@@ -1,2 +1,17 @@
-google-oauthlib-tool --scope https://www.googleapis.com/auth/assistant-sdk-prototype --scope https://www.googleapis.com/auth/gcm   --save --headless --client-secrets client_secret_454163905606-tjq3ajui0a62bcfnl48p4igueobueai4.apps.googleusercontent.com.json
-googlesamples-assistant-pushtotalk --device-id brailleprinter-f4c63 --device-model-id brailleprinter-f4c63-raspberry-us2ld5
+# google-oauthlib-tool --scope https://www.googleapis.com/auth/assistant-sdk-prototype --scope https://www.googleapis.com/auth/gcm   --save --headless --client-secrets client_secret_454163905606-tjq3ajui0a62bcfnl48p4igueobueai4.apps.googleusercontent.com.json
+# googlesamples-assistant-pushtotalk --device-id brailleprinter-f4c63 --device-model-id brailleprinter-f4c63-raspberry-us2ld5 --lang ko-KR
+
+from googlesamples.assistant.grpc import pushtotalk
+import subprocess
+
+api_endpoint = 'embeddedassistant.googleapis.com'
+project_id = 'brailleprinter-f4c63'
+device_model_id = 'brailleprinter-f4c63-raspberry-us2ld5'
+lang = 'ko-KR'
+credentials = 'client_secret_454163905606-tjq3ajui0a62bcfnl48p4igueobueai4.apps.googleusercontent.com.json'
+
+result = subprocess.check_output("googlesamples-assistant-pushtotalk --device-id brailleprinter-f4c63 --device-model-id brailleprinter-f4c63-raspberry-us2ld5 --lang ko-KR")
+print(result)
+
+#assistant.SampleAssistant('ko-KR','brailleprinter-f4c63-raspberry-us2ld5', 'brailleprinter-f4c63', None, None, None, None, None)
+    
