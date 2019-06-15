45e1c18fac72d766c48d7536fa72c4b88dcac8d9
prepola <jkh6912@gmail.com>
2019-06-15 22:33:57 +0900

    Update tts.py

--- test/tts.py
+++ test/tts.py
@@ -51,11 +51,19 @@ def run_voice(text):
     response = client.synthesize_speech(synthesis_input, voice, audio_config)
 
     # The response's audio_content is binary.
-    with open('output.mp3', 'wb') as out:
+    try:
+        with open('.\\output.mp3', 'wb') as out:
         # Write the response to the output file.
-        out.write(response.audio_content)
-    # [END tts_quickstart]  
-    playsound('output.mp3')
+            out.write(response.audio_content)
+        # [END tts_quickstart]  
+        playsound('output.mp3')
+    except:
+        with open('.\\output1.mp3', 'wb') as out:
+        # Write the response to the output file.
+            out.write(response.audio_content)
+        # [END tts_quickstart]  
+        playsound('output1.mp3')
+    
 
 if __name__ == '__main__':
     run_voice('테스트')
