83d45b3efc2a1e47ce17446c1324c4be6f9dbe05
prepola <jkh6912@gmail.com>
2019-06-17 16:25:17 +0900

    Update tts.py for multythread

--- test/tts.py
+++ test/tts.py
@@ -20,50 +20,80 @@ Example usage:
 """
 import os
 from google.cloud import texttospeech
-from playsound import playsound
+import pyaudio
+import wave
+import threading
 
-def run_voice(text):
-    # [START tts_quickstart]
-    """Synthesizes speech from the input string of text or ssml.
-    Note: ssml must be well-formed according to:
-        https://www.w3.org/TR/speech-synthesis/
-    """
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'C:\\Users\\jk691\\Documents\\hanium project-3d7b2a095e96.json'
-    # Instantiates a client
-    client = texttospeech.TextToSpeechClient()
+chunk = 1024
 
-    # Set the text input to be synthesized
-    synthesis_input = texttospeech.types.SynthesisInput(text=text)
+class make_voice(object):
+    def __init__(self, text):
+        # [START tts_quickstart]
+        """Synthesizes speech from the input string of text or ssml.
+        Note: ssml must be well-formed according to:
+            https://www.w3.org/TR/speech-synthesis/
+        """
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'C:\\Users\\jk691\\Documents\\hanium project-3d7b2a095e96.json'
+        # Instantiates a client
+        client = texttospeech.TextToSpeechClient()
 
-    # Build the voice request, select the language code ("en-US") and the ssml
-    # voice gender ("neutral")
-    voice = texttospeech.types.VoiceSelectionParams(
-        language_code='ko-KR',
-        ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+        # Set the text input to be synthesized
+        synthesis_input = texttospeech.types.SynthesisInput(text=text)
 
-    # Select the type of audio file you want returned
-    audio_config = texttospeech.types.AudioConfig(
-        audio_encoding=texttospeech.enums.AudioEncoding.MP3,
-        speaking_rate=0.8)
+        # Build the voice request, select the language code ("en-US") and the ssml
+        # voice gender ("neutral")
+        voice = texttospeech.types.VoiceSelectionParams(
+            language_code='ko-KR',
+            ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
 
-    # Perform the text-to-speech request on the text input with the selected
-    # voice parameters and audio file type
-    response = client.synthesize_speech(synthesis_input, voice, audio_config)
+        # Select the type of audio file you want returned
+        audio_config = texttospeech.types.AudioConfig(
+            audio_encoding=texttospeech.enums.AudioEncoding.LINEAR16,
+            speaking_rate=0.8)
 
-    # The response's audio_content is binary.
-    try:
-        with open('.\\output.mp3', 'wb') as out:
+        # Perform the text-to-speech request on the text input with the selected
+        # voice parameters and audio file type
+        response = client.synthesize_speech(synthesis_input, voice, audio_config)
+
+        # open the file for reading.
+        with open('.\\output.wav', 'wb') as out:
         # Write the response to the output file.
             out.write(response.audio_content)
-        # [END tts_quickstart]  
-        playsound('output.mp3')
-    except:
-        with open('.\\output1.mp3', 'wb') as out:
-        # Write the response to the output file.
-            out.write(response.audio_content)
-        # [END tts_quickstart]  
-        playsound('output1.mp3')
-    
+
+class run_voice(threading.Thread):
+    def __init__(self, name=''):
+        threading.Thread.__init__(self,name=name)
+        self.stop_event = threading.Event()
+
+    def run (self):
+        wf = wave.open('.\\output.wav', 'rb')
+
+        # create an audio object
+        p = pyaudio.PyAudio()
+
+        # open stream based on the wave object which has been input.
+        stream = p.open(format =
+                        p.get_format_from_width(wf.getsampwidth()),
+                        channels = wf.getnchannels(),
+                        rate = wf.getframerate(),
+                        output = True)
+
+        # read data (based on the chunk size)
+        data = wf.readframes(chunk)
+        while len(data) > 1:
+            # writing to the stream is what *actually* plays the sound.
+            stream.write(data)
+            data = wf.readframes(chunk)
+            if self.stop_event.is_set():
+                break
+        stream.close()    
+        p.terminate()
+
+    def stop(self): 
+        self.stop_event.set()
+        # self.join()
+
 
 if __name__ == '__main__':
-    run_voice('테스트')
+    make_voice('테스트')
+    run_voice()
