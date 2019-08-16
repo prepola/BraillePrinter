1ab8b4ceb5b867706722bf0993acabb09ca826ef
prepola <jkh6912@gmail.com>
2019-08-16 21:13:04 +0900

    [20190816] Correcting the wrong code
    ui.mode -> next_mode
    place_display -> change_display

--- test/dialog_manager.py
+++ test/dialog_manager.py
@@ -3,7 +3,7 @@ import os
 from PyQt5 import QtCore, QtGui, QtWidgets, Qt
 
 from mainwindow import Ui_Dialog as mainDialog
-from printwindow import Ui_Dialog as printDialog
+import printwindow as printDialog
 from extendwindow import Ui_Dialog as extendDialog
 
 def main():
@@ -12,7 +12,7 @@ def main():
     app = QtWidgets.QApplication(sys.argv)
 
     change_display = {
-        'print':printDialog,
+        'print':printDialog.Ui_Dialog,
         'extend':extendDialog,
         'main':mainDialog
     }
@@ -21,9 +21,9 @@ def main():
         ui = place_display(next_mode, 30)
         app.exec_()
         next_mode = ui.mode
-        print(next_mode, '로 화면이 전환됩니다.')
+        print('*'*5, next_mode, '로 화면이 전환됩니다.', '*'*5)
 
-        place_display = change_display.get(ui.mode, place_display['main'])
+        place_display = change_display.get(next_mode, change_display['main'])
 
 if __name__ == '__main__':
     main()
